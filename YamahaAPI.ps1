5e9898d887d3df97d494518f031a2468cdb2900a
Mendy <greenlighttec@users.noreply.github.com>
2022-05-30 12:53:17 -0400

    Broken Release
    Need to figure out validation range with variable (constant) use

--- YamahaAPI.ps1
+++ YamahaAPI.ps1
@@ -1,5 +1,4 @@
 ## Yamaha Receiver Powershell Module
-## Configure $Hostname in the "Invoke-YamahaAPI" function to be the IP or hostname of the receiver.
 ## Volume control is from 0-45, change the max/min ranges to go higher or lower. Note the API expects it as two digits + decimal place (-70.0db = -700)
 ## There are four scenes avaiable for configuration on the receiver call each scene by Set-YamahaScene -Scene <1-4>
 ##### THIS IS JUST FOR FUN I DONT CARE IF YOU LIKE IT
@@ -8,6 +7,7 @@
 #### Set the max DB and min DB values here
 $MaxVolume = -25
 $MinVolume = -70
+Set-Variable -Name VolumeRange -Value $MaxVolume - $MinVolume -Option Constant
 
 ### Set your hostname and protocol here
 $protocol = 'http'
@@ -54,6 +54,7 @@ $BaseDetails = $WebRequest.ChildNodes.ChildNodes.ChildNodes
 # Calculate current Volume from DB, ignoring decimal place. We're sacrificing half a decibel here
 [int]$CurrentVolume = ($BaseDetails.volume.lvl.val).Substring(0,3)
 $Volume = $CurrentVolume - $MinVolume
+if ($Volume -eq 0) {$Volume = 'MIN'} elseif ($Volume -eq $VolumeRange) {$Volume = 'MAX'}
 
 $CurrentStatus = $BaseDetails.Power_Control.power
 $AutoSleep = $BaseDetails.Power_Control.sleep
@@ -74,7 +75,7 @@ return Get-YamahaPlayStatus
 
 function Set-YamahaAudioVolume {
 param(
-[ValidateRange(0, 45)][int]$volume
+[ValidateRange(0, $VolumeRange)][int]$volume
 )
 
 # Convert the integer volume range to a dB value for the receiver, including third decimal digit.
